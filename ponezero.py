b4a2f297a5ed9b525133af2da58ae22ae1d8c46e
zishan-06-sam <mdabdulmajeed72@gmail.com>
2026-09-08 15:25:33 +0530

    Add files via upload

---
+++ ponezero.py
@@ -0,0 +1,11 @@
+A = int(input("Enter any number :"))
+if A > 0:
+    print("The number is positive.")
+elif A < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+if A % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
