0af046495227b63ac99e7b0c490c04a19fa0a5ff
zishan-06-sam <mdabdulmajeed72@gmail.com>
2026-09-08 10:52:59 +0530

    Add files via upload

---
+++ threeequal.py
@@ -0,0 +1,7 @@
+A = int(input("Enter first number"))
+B = int(input("Enter second number"))
+C = int(input("Enter third number"))
+if A == B and B == C:
+    print("All three numbers are equal")
+else:
+    print("All three numbers are not equal")
