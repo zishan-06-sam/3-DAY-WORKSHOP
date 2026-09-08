3793d596adf69d8a1a9ab778148deeac4afb7a24
zishan-06-sam <mdabdulmajeed72@gmail.com>
2026-09-08 10:48:27 +0530

    Add files via upload

---
+++ greaternum.py
@@ -0,0 +1,6 @@
+A = int(input("Enter first number:"))
+B = int(input("Enter second number:"))
+if A > B:
+  print("The first number is greater than the second number")
+else:
+  print("The second number is greater than the first number")
