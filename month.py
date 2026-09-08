96c1552082b4ea9611d31b333b0aadd5306323df
zishan-06-sam <mdabdulmajeed72@gmail.com>
2026-09-08 15:18:55 +0530

    Add files via upload

---
+++ month.py
@@ -0,0 +1,27 @@
+A = int(input("Enter the month"))
+if A == 1:
+    print("January (31 days)")
+elif A == 2:
+    print("February (28 or 29 days)")
+elif A == 3:
+    print("March (31 days)")
+elif A == 4:
+    print("April (30 days)")
+elif A == 5:
+    print("May (31 days)")
+elif A == 6:
+    print("June (30 days)")
+elif A == 7:
+    print("July (31 days)")
+elif A == 8:
+    print("August (31 days)")
+elif A == 9:
+    print("September (30 days)")
+elif A == 10:
+    print("October (31 days)")
+elif A == 11:
+    print("November (30 days)")
+elif A == 12:
+    print("December (31 days)")
+else:
+    print("Invalid month number")
