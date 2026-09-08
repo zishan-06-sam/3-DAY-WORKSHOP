41b96b57e0a9c2a7b88da6572946a97642f25c86
zishan-06-sam <mdabdulmajeed72@gmail.com>
2026-09-08 12:07:26 +0530

    Add files via upload

---
+++ marksgrade.py
@@ -0,0 +1,12 @@
+A = float(input("Enter marks: "))
+if A >= 90:
+    print("Grade: A")
+elif A >= 80:
+    print("Grade: B")
+elif A >= 70:
+    print("Grade: C")
+elif A >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+    
